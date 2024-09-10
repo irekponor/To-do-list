c0a02e8c22e5c16264f877e9702915e580ebd035
emmanuel <emmanuelirekponor86@gmail.com>
2024-09-10 02:51:46 +0100

    first change

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-do List</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>To-do List</h1>
+        <form action="index.php" method="post">
+            <input type="text" name="task" placeholder="Enter new task.." id="">
+            <button type="submit" name="addtask"></button>
+        </form>
+    </div>
+</body>
+
+</html>
